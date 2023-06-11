913ac3146d34420320f3a7ac36f4a3cb64c72bd1
Adielson Medeiros <medeirosadielson@gmail.com>
2023-06-11 15:49:54 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+export default function Home() {
+    const navigation = useNavigation()
+ return (
+   <View style={{ marginTop: 20 }}>
+    <Text>Home</Text>
+    <Button title="Contato" onPress={ ()=> navigation.navigate('Contato') }/>
+   </View>
+  );
+}
